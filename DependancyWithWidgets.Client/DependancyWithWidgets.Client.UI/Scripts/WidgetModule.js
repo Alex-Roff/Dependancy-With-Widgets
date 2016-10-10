c9df95efa96a93c8e7f1e60080b2f4a5021c2beb
Alex Roff <alex.roff.career@gmail.com>
2016-10-10 17:05:49 -0400

    Final changes 10/10/2016 5:05pm

---
+++ DependancyWithWidgets.Client/DependancyWithWidgets.Client.UI/Scripts/WidgetModule.js
@@ -0,0 +1,20 @@
+﻿(function (wa) {
+  'using strict';
+
+  wa.angular.controller('widgetController', ['$scope', 'widgetService', function ($scope, widgetService) {
+
+
+    $scope.model = {
+      Name: {}
+    };
+
+
+    genreService.get(function (responce) {
+      $scope.genres = responce.data;
+    }, function (error) {
+      console.log(error);
+    });
+
+  }]);
+
+})(window.webApp);
---
+++ DependancyWithWidgets.Client/DependancyWithWidgets.Client.UI/Scripts/WidgetService.js
@@ -0,0 +1,22 @@
+﻿(function (wa) {
+  'using strict';
+
+  wa.angular.factory('widgetService', ['$http', function ($http) {
+    return {
+      get: function (pass, fail) {
+        $http({
+          method: 'get',
+          url: 'http://localhost/GreenTomatoRest/api/genre'
+        }).then(pass, fail);
+      },
+      post: function (data, pass, fail) {
+        $http({
+          method: 'post',
+          data: data,
+          url: 'http://localhost/GreenTomatoRest/api/genre'
+        }).then(pass, fail);
+      }
+    }
+  }]);
+
+})(window.webApp);
