d368e392fab4997a0756a646238b7409031cd75d
Alex Roff <alex.roff.career@gmail.com>
2016-10-10 13:56:12 -0400

    Added Soap service.

--- DependancyWithWidgets.Data/DependancyWithWidgets.Data.Soap/Properties/AssemblyInfo.cs
+++ DependancyWithWidgets.Data/DependancyWithWidgets.Data.Soap/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06326916-c481-46c4-acb3-627a9c5a16f0")]
+[assembly: Guid("0e0c0212-879d-41c5-bdc6-16ae3465c5e6")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
